30fdbf582ad4520752385d3b33e08bd5f7871f53
samuel <samuelgomes101112@gmail.com>
2025-09-18 14:55:40 -0300

    modify text in form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,7 +4,7 @@
       <h2 class="text-2xl font-bold text-gray-800 mb-3 text-center">Indique um amigo!</h2>
       <p class="text-sm text-gray-600 text-center mb-6">
         Preencha todos os campos abaixo para <br/>
-        concorrer a um <span class="font-bold text-[#d87812]">Desconto de até 100%, Alexa, Table e um Kit uniforme</span>.
+        concorrer a um <span class="font-bold text-[#d87812]">Desconto de até 100%, Alexa, Tablet e um Kit uniforme</span>.
       </p>
 
       <FormRegister/>
